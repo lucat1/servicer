df717b42c7bc32bff660dcd1465d59ed228f40b8
Luca <LucaT1@users.noreply.github.com>
2017-11-26 14:35:50 +0100

    :shit: Quick fix for more comple modules
    Related to: #1

--- src/spawner.ts
+++ src/spawner.ts
@@ -1,54 +1,11 @@
-import { readFileSync } from 'fs'
+import { readFileSync, existsSync } from 'fs'
 import { runInNewContext, createContext } from 'vm'
 import { join } from 'path'
+import { IRoute } from './interfaces'
 
-export const getFunction = route => {
-  const mod:  { 
-    exports?: Function | any
-    __esModule?: boolean
-  } = {}
-
-  const context = createContext({
-    module: mod,
-    clearImmediate: clearImmediate,
-    clearInterval: clearInterval,
-    clearTimeout: clearTimeout,
-    setImmediate: setImmediate,
-    setInterval: setInterval,
-    setTimeout: setTimeout,
-    console: {},
-    process: {
-      env: {
-        ...process.env,
-        NODE_PATH: route.path
-      },
-      __path: '/',
-      cwd: path => path ? this.__path = path : this.__path 
-    },
-    Buffer: Buffer,
-    require: (path) => {
-      switch(path) {
-        case 'fs':
-        case 'net':
-        case 'process':
-        case 'http': 
-        case 'https':
-          throw new Error('You cannot yet require this module for security reasons.')
-
-        default:
-          const modulePath = join(route.path, 'node_modules', path)
-
-          return require(modulePath)
-      } 
-    }
-  })
-
-  const pkg = require(join(route.path, 'package.json'))
-  const content = readFileSync(join(route.path, pkg.main))
-
-  runInNewContext(content.toString(), context)
-
-  const result = mod.__esModule ? mod.exports.default : mod.exports
+export const getFunction = (route: IRoute) => {
+  const _exports = require(route.path)
+  const result = _exports.__esModule ? _exports.default : _exports
 
   return result
 }
