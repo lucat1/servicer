61da6221fefbe0823088bf409d329ae4a7e83dee
Luca <LucaT1@users.noreply.github.com>
2017-11-30 20:00:21 +0100

    :art: Log server's ip address

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,7 @@
+import { address } from 'ip'
+
 export const API_PORT    = process.env.API_PORT    || 8080
 export const SOCKET_PORT = process.env.SOCKET_PORT || 3434
+export const ADDRESS     = address()
 export const NPM_CLIENT  = process.env.NPM_CLIENT  || 'yarn'
 export const NPM_INSTALL = JSON.parse(process.env.NPM_INSTALL || '["install", "--json"]')
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,10 @@ import * as rd from 'readline'
 import db from './db'
 import { openSocket } from './socket'
 import { openApi } from './http'
-import { API_PORT, SOCKET_PORT } from './constants'
+import { API_PORT, SOCKET_PORT, ADDRESS } from './constants'
 
-openSocket(any => console.log('The socket is listening on port', SOCKET_PORT))
-openApi(any => console.log('The https api is listening on port', API_PORT))
+openSocket(any => console.log('The socket is listening on port socket://%s:%s', ADDRESS, SOCKET_PORT))
+openApi(any => console.log('The https api is listening on port http://%s:%s', ADDRESS, API_PORT))
 
 rd.createInterface(process.stdin, process.stdout)
   .on('line', (input: string) => input == 'db' && console.log(JSON.stringify(db.getState(), null, 2)))
